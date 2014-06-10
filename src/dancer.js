bacaaeac1510d83694a99e35aa59ffe3ca49ae25
Hack Reactor Students <james.yothers@gmail.com>
2014-06-09 21:29:37 -0700

    Many blinking dancers

--- src/dancer.js
+++ src/dancer.js
@@ -6,7 +6,7 @@ var makeDancer = function(top, left, timeBetweenSteps){
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   this.step();
-
+  this.setPosition(top,left);
 };
 
 makeDancer.prototype.step = function(){
@@ -35,7 +35,7 @@ makeDancer.prototype.setPosition = function(top, left){
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  this.setPosition(top, left);
+  //this.setPosition(top, left);
 };
 
 

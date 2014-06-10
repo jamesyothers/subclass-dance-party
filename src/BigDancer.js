99f8739b8131c271feed7e3bf91dc29ce92b2928
Hack Reactor Students <james.yothers@gmail.com>
2014-06-10 10:45:10 -0700

    Big blue fadded added

--- src/BigDancer.js
+++ src/BigDancer.js
@@ -15,5 +15,5 @@ BigDancer.prototype.step = function(){
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.toggle();
+  this.$node.fadeToggle();
 };
